73b5d18d9f284cb12a397052199534cf32fb0deb
IsabelaGomez101 <isa.gomc@gmail.com>
2022-02-06 22:02:34 -0500

    adding file export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format.
+'''
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/" + id
+    url2 = "https://jsonplaceholder.typicode.com/todos?userId=" + id
+    users = requests.get(url, verify=False).json()
+    tasks = requests.get(url2, verify=False).json()
+    with open("{}.csv".format(id), 'w', newline='') as file:
+        task_w = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            task_w.writerow([int(id), users.get('username'),
+                            task.get('completed'), task.get('title')])
